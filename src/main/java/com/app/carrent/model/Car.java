f737d71594a609710fb928a1ba79d11d53e28354
Stanislaw Gniatczyk <gniatczyk@gmail.com>
2020-05-24 10:36:05 +0200

    Added car model

---
+++ src/main/java/com/app/carrent/model/Car.java
@@ -0,0 +1,30 @@
+package com.app.carrent.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String mark;
+    private String model;
+    private int year;
+    private String description;
+    private double pricePerDay;
+    private double pricePerKm;
+
+    public Car() {
+    }
+}

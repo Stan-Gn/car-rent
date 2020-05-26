a1bf0b668239272dda24d9f6ecf9ee1a76917a15
Stanislaw Gniatczyk <gniatczyk@gmail.com>
2020-05-26 09:51:23 +0200

    Added to car model car body type

--- src/main/java/com/app/carrent/model/Car.java
+++ src/main/java/com/app/carrent/model/Car.java
@@ -1,6 +1,5 @@
 package com.app.carrent.model;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,9 +20,15 @@ public class Car {
     private String mark;
     private String model;
     private int year;
+    private CarType carType;
     private String description;
     private double pricePerDay;
     private double pricePerKm;
+
+
+    public enum CarType {
+        KOMBI, COUPE, SEDAN, SUV, HATCHBACK
+    }
 
     public Car() {
     }

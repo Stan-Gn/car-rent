597ef5347956ab58fdb2cae9e6497bec3d78e9ca
Stanislaw Gniatczyk <gniatczyk@gmail.com>
2020-05-26 21:15:57 +0200

    Changes in the car service to support the controller - find all cars and find car by id

--- src/main/java/com/app/carrent/service/CarService.java
+++ src/main/java/com/app/carrent/service/CarService.java
@@ -2,27 +2,32 @@ package com.app.carrent.service;
 
 
 import com.app.carrent.model.Car;
-import com.app.carrent.repository.CarRepository;
+import com.app.carrent.repository.CarRepositoryInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CarService {
 
-    private CarRepository carRepository;
+    private CarRepositoryInterface carRepository;
 
     @Autowired
-    public CarService(CarRepository carRepository) {
+    public CarService(CarRepositoryInterface carRepository) {
         this.carRepository = carRepository;
     }
 
-    public Car findCarById (long id){
-        return carRepository.findCarById(id);
+    public List<Car> getAllCars() {
+        return carRepository.findAll();
     }
 
-    public List<Car> getAllCars (){
-        return carRepository.getAllCars();
+    public Car findCarById(long id) {
+        //todo exception handling
+        Optional<Car> car = carRepository.findById(id);
+        return car.get();
     }
+
 }

f3ead061d7b699df8918e835be3dcf210ad4f777
Stanislaw Gniatczyk <gniatczyk@gmail.com>
2020-06-02 16:13:24 +0200

    car list pagination

--- src/main/java/com/app/carrent/controller/CarRentController.java
+++ src/main/java/com/app/carrent/controller/CarRentController.java
@@ -1,10 +1,22 @@
 package com.app.carrent.controller;
 
+import com.app.carrent.model.Car;
 import com.app.carrent.service.CarService;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.relational.core.sql.In;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class CarRentController {
@@ -24,9 +36,23 @@ public class CarRentController {
     }
 
     @GetMapping("/cars")
-    private ModelAndView cars() {
+    private ModelAndView cars(@RequestParam("page") Optional<Integer>page) throws NotFoundException {
         ModelAndView modelAndView = new ModelAndView("car-list-one");
-        modelAndView.addObject("cars", carService.findAll());
+
+        int currentPage = page.orElse(1);
+        Page<Car> carsPage = carService.findAll(PageRequest.of(currentPage -1,4));
+        if(currentPage>carsPage.getTotalPages())
+            throw new NotFoundException("Page not found");
+
+        modelAndView.addObject("carsPage", carsPage);
+
+        int totalPages = carsPage.getTotalPages();
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            modelAndView.addObject("pageNumbers", pageNumbers);
+        }
         return modelAndView;
     }
 
